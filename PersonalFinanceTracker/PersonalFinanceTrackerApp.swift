e91661bf95feffd612f35973e138ecb7de3a3e16
Keerthana Ramesh <kr36621@uga.edu>
2024-12-02 19:35:16 -0500

    Initial Commit

---
+++ PersonalFinanceTracker/PersonalFinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanceTrackerApp.swift
+//  PersonalFinanceTracker
+//
+//  Created by Keerthana Ramesh on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PersonalFinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PersonalFinanceTrackerTests/PersonalFinanceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanceTrackerTests.swift
+//  PersonalFinanceTrackerTests
+//
+//  Created by Keerthana Ramesh on 12/2/24.
+//
+
+import Testing
+@testable import PersonalFinanceTracker
+
+struct PersonalFinanceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
